59d40f05ba0f8e00e45b121e96f41904e616e923
Elias Achi <elias@plazacybersecurity.com>
2023-07-03 21:10:38 -0500

    Fixes typo on command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run Celery Worker
-celery app.scheduler.celery worker --loglevel=info -E --detach --pidfile=''
+celery -A app.scheduler.celery worker --loglevel=info -E --detach --pidfile=''
 # Run Celery Beat
-celery app.scheduler.celery beat --loglevel=info --detach --pidfile=''
+celery -A app.scheduler.celery beat --loglevel=info --detach --pidfile=''
 # Run Flask App (PPMCore)
 flask run --host=0.0.0.0 --port=5000
