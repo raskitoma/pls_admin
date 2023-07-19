76052eda9c7ad5e3a90e31bab7a603fe147e9ced
Elias Achi <elias@plazacybersecurity.com>
2023-07-19 10:01:04 -0500

    Added changes suggested by @terry-sydaus

--- app/scheduler_tasks.py
+++ app/scheduler_tasks.py
@@ -33,25 +33,32 @@ from app.rskcore.models import pls_wallets, \
 REWARD_BASE_PCT = app.config['REWARD_BASE_PCT']
 PLS_PRICE_URI = app.config['PLS_PRICE_URI']
 PLS_PRICE_API_KEY = app.config['PLS_PRICE_API_KEY']
+PLS_PRICE_FX = app.config['PLS_PRICE_FX']
 
 # get price from provider
-def get_price(uri, api_key):
+def get_price(uri, api_key, fx_param):
     headers = {'X-CMC_PRO_API_KEY': api_key}
+    parameters = {'convert': fx_param}
     try:
-        response = requests.get(uri, headers=headers)
-        data = json.loads(response.text)
+        response1 = requests.get(uri, headers=headers)
+        response2 = requests.get(uri, headers=headers, params=parameters)
+        data1 = json.loads(response1.text)
+        data2 = json.loads(response2.text)
     except Exception as e:
         log2store = f"{get_time()} | Error getting data from price API: {str(e)}"
         prRed(f'{get_time()} | {log2store}')
         price_usd = pls_price.get_last().priceUSD
-        return price_usd
+        price_fx = pls_price.get_last().priceFX
+        return price_usd, price_fx
     try:
-        price_usd = float(data['data']['11145']['quote']['USD']['price'])
+        price_usd = float(data1['data']['11145']['quote']['USD']['price'])
+        price_fx = float(data2['data']['11145']['quote'][fx_param]['price'])
     except Exception as e:
         log2store = f"{get_time()} | Error getting price from API: {str(e)}"
         prRed(f'{get_time()} | {log2store}')
         price_usd = pls_price.get_last().priceUSD
-    return price_usd
+        price_fx = pls_price.get_last().priceFX
+    return price_usd, price_fx
 
 # ###############################################################################
 # setting up tasks
@@ -65,7 +72,7 @@ def pls_price_update():
     new_log(users_id=1, module='PRICE_UPDATE', severity=SEV_INF, description=log2store, data=log2store, image=None)
     # get PLS price
     try:
-        price_usd = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY)
+        price_usd, price_fx = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY, PLS_PRICE_FX)
     except Exception as e:
         log2store = f"{get_time()} | Error getting data from price API: {str(e)}"
         prRed(f'{get_time()} | {log2store}')
@@ -74,7 +81,7 @@ def pls_price_update():
     if everything_ok:
         # store price in db
         try:
-            pls_price.store_new_price(price_usd)
+            pls_price.store_new_price(price_usd, price_fx)
         except Exception as e:
             log2store = f"{get_time()} | Error storing price in db: {str(e)}"
             prRed(f'{get_time()} | SCHEDULER ==> {log2store}')
@@ -90,6 +97,7 @@ def wallets_review():
     new_log(users_id=1, module='PLS_WALLET', severity=SEV_INF, description=log2store, data=log2store, image=None)
     # get all wallets
     pls_wallets_list = pls_wallets.query.all()
+    price_usd, price_fx = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY, PLS_PRICE_FX)
     for wallet in pls_wallets_list:
         wallet_data_uri = f'https://scan.pulsechain.com/api?module=account&action=balance&address={wallet.address}'
         wallet_data = requests.get(wallet_data_uri)
@@ -99,8 +107,22 @@ def wallets_review():
                 # save new value for history purposes
                 current_balance = wallet_info['result']
                 previous_balance = pls_wallets.get_balance(wallet.address)
-                if current_balance != previous_balance:
-                    pls_wallet_history.new_balance(wallet.address, current_balance)
+                if float(current_balance) != previous_balance:
+                    if float(current_balance) > previous_balance:
+                        balance_increase = float(current_balance) - previous_balance
+                        if balance_increase < 32000000:
+                            #increase in balance from validator rewards and/or fee recipient tips/rewards
+                            taxableIncome_USD = balance_increase * price_usd
+                            taxableIncome_FX = balance_increase * price_fx
+                        else:
+                            #increase in balance caused by exiting validator/s with or without validator
+                            #rewards and/or fee recipient tips/rewards
+                            taxableIncome_USD = (balance_increase % 32000000) * price_usd
+                            taxableIncome_FX = (balance_increase % 32000000) * price_fx
+                    else:
+                        taxableIncome_USD = 0
+                        taxableIncome_FX = 0            
+                    pls_wallet_history.new_balance(wallet.address, current_balance, price_usd, price_fx, taxableIncome_USD, taxableIncome_FX)
                     # update wallet balance
                     pls_wallets.update_balance(wallet.address, current_balance)
                     log2store = f'Wallet {wallet.address} updated with balance {current_balance}'
@@ -128,7 +150,7 @@ def validator_update():
     block_last_height = app.config['MAX_HEIGHT_CHECK'] if block_last_managed is None else block_last_managed.blockheight
     # get pls current price, we don't need to much precision, it's good enough to get this price on this run for all withdrawals found
     # and also is good to no stress and overload the API
-    price_usd = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY)
+    price_usd, price_fx = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY, PLS_PRICE_FX)
     # get wallets
     wallets = pls_wallets.get_all()
     for wallet in wallets:
@@ -154,7 +176,8 @@ def validator_update():
                             w_miner = block_data.miner
                             w_timestamp = datetime.datetime.fromtimestamp(int(block_data.timestamp))
                             w_address = wallet.address
-                            w_price = price_usd
+                            w_price_usd = price_usd
+                            w_price_fx = price_fx
                             new_withdrawal_data = pls_validator_withdrawals(
                                 index = w_index,
                                 validatorIndex = w_validatorIndex,
@@ -163,7 +186,8 @@ def validator_update():
                                 miner = w_miner,
                                 timeStamp = w_timestamp,
                                 address = w_address,
-                                priceUSD = w_price
+                                priceUSD = w_price_usd,
+                                priceFX = w_price_fx
                             )
                             db.session.add(new_withdrawal_data)
                             try:
@@ -195,7 +219,7 @@ def pls_custom_sync(xfrom, xto):
     web3 = Web3(Web3.HTTPProvider(app.config['WEB3_PROVIDER_URI']))
     # get pls current price, we don't need to much precision, it's good enough to get this price on this run for all withdrawals found
     # and also is good to no stress and overload the API
-    price_usd = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY)
+    price_usd, price_fx = get_price(PLS_PRICE_URI, PLS_PRICE_API_KEY, PLS_PRICE_FX)
     # get wallets    
     wallets = pls_wallets.get_all()    
     for wallet in wallets:
@@ -218,7 +242,8 @@ def pls_custom_sync(xfrom, xto):
                         w_miner = block_data.miner
                         w_timestamp = datetime.datetime.fromtimestamp(int(block_data.timestamp))
                         w_address = wallet.address
-                        w_price = price_usd
+                        w_price_usd = price_usd
+                        w_price_fx = price_fx
                         new_withdrawal_data = pls_validator_withdrawals(
                             index = w_index,
                             validatorIndex = w_validatorIndex,
@@ -227,7 +252,8 @@ def pls_custom_sync(xfrom, xto):
                             miner = w_miner,
                             timeStamp = w_timestamp,
                             address = w_address,
-                            priceUSD = w_price
+                            priceUSD = w_price_usd,
+                            priceFX = w_price_fx
                         )
                         db.session.add(new_withdrawal_data)
                         try:
